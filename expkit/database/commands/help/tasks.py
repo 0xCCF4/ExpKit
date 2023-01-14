8b75c329604e5ec9f1492971ba49d9e08906b60e
Johannes Lenzen <johannes.lenzen@twinsec.de>
2023-01-14 11:08:52 +0100

    Migrated help tasks command to new argparse system

--- expkit/database/commands/help/tasks.py
+++ expkit/database/commands/help/tasks.py
@@ -1,5 +1,6 @@
+import argparse
 import textwrap
-from typing import Optional, get_type_hints
+from typing import Optional, get_type_hints, List, Tuple
 
 from expkit.base.command.base import CommandTemplate, CommandOptions
 from expkit.base.logger import get_logger
@@ -9,7 +10,12 @@ LOGGER = get_logger(__name__)
 PRINT = get_logger(__name__, True)
 
 
-# todo migrate to argparse system
+class HelpOptions(CommandOptions):
+    def __init__(self):
+        super().__init__()
+        self.help_tasks: List[str] = []
+
+
 @register_command
 class TaskInfoCommand(CommandTemplate):
     def __init__(self):
@@ -20,21 +26,36 @@ class TaskInfoCommand(CommandTemplate):
             of all tasks, is printed.
             When a name is given, the help for the specific task is printed.
             This includes a description and a list of available config parameters.
-            '''))
+            '''), options=HelpOptions)
 
-    def execute(self, options: CommandOptions, *args) -> bool:
+    def create_argparse(self) -> argparse.ArgumentParser:
+        parser = super().create_argparse()
+
+        parser.add_argument("name", nargs="*", default=None, help="Name of the task (or 'all') to print information about.")
+
+        return parser
+
+    def parse_arguments(self, *args: str) -> Tuple[HelpOptions, argparse.ArgumentParser, argparse.Namespace]:
+        options, parser, namespace = super().parse_arguments(*args)
+
+        options.help_tasks = namespace.name
+
+        if "all" in options.help_tasks:
+            db = TaskDatabase.get_instance()
+            options.help_tasks = tuple(sorted([s.name for s in db.tasks.values()]))
+
+        return options, parser, namespace
+
+    def execute(self, options: HelpOptions) -> bool:
         db = TaskDatabase.get_instance()
 
-        if len(args) == 1 and args[0] == "all":
-            args = tuple(sorted([s.name for s in db.tasks.values()]))
-
-        if len(args) == 0:
+        if len(options.help_tasks) == 0:
             PRINT.info(f"Printing list of all tasks")
             for task in sorted([task.name for task in db.tasks.values()]):
                 PRINT.info(f" - {task}")
             PRINT.info("")
         else:
-            for name in args:
+            for name in options.help_tasks:
                 task = db.get_task(name)
                 if task is None:
                     LOGGER.error(f"Task not found: {name}")
